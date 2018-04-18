377192050b974bff5e0d59bbfee5d5105e885077
david <lexuszhi1990@gmail.com>
2018-04-18 17:39:38 +0800

    update code

--- benchmark.sh
+++ benchmark.sh
@@ -1,6 +1,6 @@
 # export MXNET_CUDNN_AUTOTUNE_DEFAULT=0
 
-num_gpus=0
+num_gpus=5
 num_workers=16
 batch_size=20
 default_epochs=40
@@ -17,8 +17,8 @@ default_model=densenet201
 # python3 train_task.py --task collar_design_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
 # python3 train_task.py --task neckline_design_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
 # python3 train_task.py --task skirt_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
-python3 train_task.py --task sleeve_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
+# python3 train_task.py --task sleeve_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
 # python3 train_task.py --task neck_design_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
 # python3 train_task.py --task coat_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
 # python3 train_task.py --task lapel_design_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
-# python3 train_task.py --task pant_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
+python3 train_task.py --task pant_length_labels --model $default_model --num-gpus $num_gpus -j $num_workers -b $batch_size --epochs $default_epochs
