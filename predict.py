a2e7b1764b29a5ecabbd57a598c159d532cb5de2
david <lexuszhi1990@gmail.com>
2018-04-20 20:42:57 +0800

    update predict

--- predict.py
+++ predict.py
@@ -14,17 +14,18 @@ from pathlib import Path
 from solver import Solver
 from src.config import config
 
-VERSION = 'v3'
+VERSION = 'v2'
 model_dict = config.MODEL_LIST[VERSION]
 task_list = ['collar_design_labels', 'skirt_length_labels', 'lapel_design_labels', 'neckline_design_labels', 'coat_length_labels', 'neck_design_labels', 'pant_length_labels', 'sleeve_length_labels']
 
 test_dataset_path = '/data/david/fai_attr/transfered_data/test_v1'
+submission_path = None
 # submission_path = '/data/david/fai_attr/transfered_data/test_v1/submissions/test_v2'
-submission_path = '/data/david/fai_attr/gloun_data/submission/2018-04-20-11-42-74877'
 
 batch_size=4
 num_workers=2
-gpus = [0]
+gpus = [2]
+cropped_predict=False
 
 if len(sys.argv) == 2:
     task = sys.argv[1]
@@ -32,9 +33,9 @@ if len(sys.argv) == 2:
     assert task in task_list, "UNKOWN TASK"
     details = model_dict[task]
     solver = Solver(batch_size=batch_size, num_workers=num_workers, gpus=gpus, submission_path=submission_path)
-    solver.predict(test_dataset_path, model_path=details['model_path'], task=task, network=details['network'])
+    solver.predict(test_dataset_path, model_path=details['model_path'], task=task, network=details['network'], cropped_predict=cropped_predict)
 else:
     solver = Solver(batch_size=batch_size, num_workers=16, gpus=gpus, submission_path=submission_path)
     for index, task in enumerate(model_dict):
         details = model_dict[task]
-        solver.predict(test_dataset_path, model_path=details['model_path'], task=task, network=details['network'])
+        solver.predict(test_dataset_path, model_path=details['model_path'], task=task, network=details['network'], cropped_predict=cropped_predict)
