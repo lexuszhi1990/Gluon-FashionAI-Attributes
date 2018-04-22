00542d9de7489819e9660f5c1709dd6f2a74bdf7
david <lexuszhi1990@gmail.com>
2018-04-22 17:29:59 +0800

    update train.py with gpu

--- train.py
+++ train.py
@@ -20,7 +20,8 @@ from src.config import config
 VERSION = 'v4'
 model_dict = config.MODEL_LIST[VERSION]
 task_list = ['collar_design_labels', 'skirt_length_labels', 'lapel_design_labels', 'neckline_design_labels', 'coat_length_labels', 'neck_design_labels', 'pant_length_labels', 'sleeve_length_labels']
-training_path = "/data/david/fai_attr/transfered_data/train_v4"
+training_path = "/data/david/fai_attr/transfered_data/train_v6"
+validation_path = "/data/david/fai_attr/transfered_data/val_v6"
 # os.environ['CUDA_VISIBLE_DEVICES'] = str(details['gpu'])
 
 if len(sys.argv) == 2:
@@ -28,5 +29,5 @@ if len(sys.argv) == 2:
     assert task in task_list, "UNKOWN TASK"
     print("start predict single task: %s" % task)
     details = model_dict[task]
-    solver = Solver(training_path=training_path)
+    solver = Solver(training_path=training_path, validation_path=validation_path)
     solver.train(task=task, model_name=details['network'], epochs=details['epochs'], lr=details['lr'], momentum=details['momentum'], wd=details['wd'], lr_factor=details['lr_factor'], lr_steps=details['lr_steps'], gpus=details['gpus'], batch_size=details['batch_size'], num_workers=details['num_workers'])
